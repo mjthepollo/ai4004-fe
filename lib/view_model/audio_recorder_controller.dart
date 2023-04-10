94dcb10b83f122364776b166b029fcc5967b907b
ttajun36 <ttajun36@gmail.com>
2023-04-10 14:30:07 +0900

    Feat: JSON message배열 추가

--- lib/view_model/audio_recorder_controller.dart
+++ lib/view_model/audio_recorder_controller.dart
@@ -5,9 +5,13 @@ import 'dart:io';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:http/http.dart' as http;
 
+import '../object/message.dart';
+
 class AudioRecorderController {
   final FlutterSoundRecorder _recorder = FlutterSoundRecorder();
+
   String? _recordingFilePath;
+  List<Message>? messageList = [];
 
   Future<void> startRecording() async {
     await _recorder.openRecorder();
@@ -22,23 +26,75 @@ class AudioRecorderController {
     return _recordingFilePath;
   }
 
-  Future<bool> sendAudioData(String filePath) async {
+  Future<String> sendAudioData(String filePath) async {
     final audioFileBytes = await File(filePath).readAsBytes();
     final base64AudioFile = base64Encode(audioFileBytes);
+
+    //messageList to Json
+    List<Map<String, String>> messageListJson = messageList
+            ?.map((message) => {
+                  'role': message.role,
+                  'content': message.content,
+                })
+            .toList() ??
+        [];
 
     final response = await http.post(
       Uri.parse(
           'https://wgmywho6v8.execute-api.ap-northeast-2.amazonaws.com/v1/answer'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'audio': base64AudioFile}),
+      body: jsonEncode({
+        'audio': base64AudioFile,
+        'messages': messageListJson,
+      }),
     );
 
     if (response.statusCode == 200) {
+      print('200is right');
+
+      final jsonResponse = jsonDecode(response.body);
+      final audioUrl = jsonResponse['audio_url'];
+      final messageListJson = jsonResponse['messages'];
+      final List<Message> updatedMessageList = [];
+
+      //receive json and update messageList
+      if (messageListJson != null) {
+        messageListJson.forEach((messageJson) {
+          final message = Message(
+            role: messageJson['role'],
+            content: messageJson['content'],
+          );
+          updatedMessageList.add(message);
+        });
+      }
+
+      // Update messageList with updatedMessageList
+      messageList = updatedMessageList;
+
+      //for test -> messageList를 json형태로 볼 수 있음.
+      if (messageList != null) {
+        // Convert messageList to JSON string
+        List<Map<String, String>> messageListJson = messageList
+                ?.map((message) => {
+                      'role': message.role,
+                      'content': message.content,
+                    })
+                .toList() ??
+            [];
+
+        print(messageListJson);
+      }
+
       log(response.body);
-      return true;
+      if (audioUrl == null) {
+        return '';
+      } else {
+        return audioUrl;
+      }
     } else {
+      print('not 200');
       log(response.body);
-      return false;
+      return '';
     }
   }
 
